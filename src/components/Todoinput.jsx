19d5c1ecff92d32cb3770e68c6ee5fdf8652a4ee
smatsu-5 <59785824+smatsu-5@users.noreply.github.com>
2024-06-11 18:46:34 -0400

    Update Todoinput.jsx

--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -8,7 +8,7 @@ export default function Todoinput(props) {
     <header>
         <input value={todoValue} onChange={(e) => {
                 setTodoValue(e.target.value)
-            }} placeholder="Enter a new todo..." />
+            }} placeholder="Enter a new todo...." />
             <button onClick={() => {
                 handleAddTodos(todoValue)
                 setTodoValue('')
